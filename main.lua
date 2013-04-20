1e8c961adc303cb9da892a6f83deaf8ee1ee217c
Lyndon Maydwell <lyndon@sordina.net>
2013-04-20 21:47:43 +0800

    Better bullets

--- main.lua
+++ main.lua
@@ -1,9 +1,17 @@
 function love.draw()
-	love.graphics.translate(x, y)
 	love.graphics.push()
+	love.graphics.translate(x, y)
 	love.graphics.rotate(theta)
-	love.graphics.print("Hello -=>" .. bullets,0,0)
+	love.graphics.print("Hello -=>",0,0)
 	love.graphics.pop()
+
+	for k,v in pairs(bullets) do
+		love.graphics.push()
+		love.graphics.translate(v['x'], v['y'])
+		love.graphics.rotate(v['theta'])
+		love.graphics.print(v['bullet'],0,0)
+		love.graphics.pop()
+	end
 end
 
 function love.mousepressed(x, y, button)
@@ -17,18 +25,31 @@ function love.keypressed(key,unikey)
 	end
 
 	if unikey == 32 then -- space
-		bullets = "=>" .. bullets
-		bling = love.audio.newSource("audio/gun.wav")
-		love.audio.play(bling)
+		table.insert(bullets, {x=x + math.cos(theta) * 50, y=y + math.sin(theta) * 50, theta=theta, bullet="=>"})
+		love.audio.rewind(gun)
+		love.audio.play(gun)
 	end
+end
+
+function delta(x1,y1,x2,y2)
+	dx = x1 - x2
+	dy = y1 - y2
+	a  = math.pow(dx,2)
+	b  = math.pow(dy,2)
+	return math.sqrt(a+b)
 end
 
 function love.update(dt)
 	x = x + dt * speed * math.cos(theta)
 	y = y + dt * speed * math.sin(theta)
 
-	if math.random() > 0.9 then
-		bullets = " " .. bullets
+	for k,v in pairs(bullets) do
+		if delta(v['x'],v['y'],x,y) < 2000 then
+			v['x'] = v['x'] + dt * 2 * speed * math.cos(v['theta'])
+			v['y'] = v['y'] + dt * 2 * speed * math.sin(v['theta'])
+		else
+			table.remove(bullets,k)
+		end
 	end
 
 	if love.keyboard.isDown("left") then
@@ -44,10 +65,11 @@ function love.load()
 	init_player()
 	init_bullets()
 	background_music()
+	gun = love.audio.newSource("audio/gun.wav", "static")
 end
 
 function init_bullets()
-	bullets = ""
+	bullets = {}
 end
 
 function init_player()
