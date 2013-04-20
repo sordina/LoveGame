e55017257b648407ae74bb87bef7dad362359eb5
Lyndon Maydwell <lyndon@sordina.net>
2013-04-20 21:21:52 +0800

    Bullets

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@ help:
 	@cat Makefile | grep -i '^[a-z]*:' | sed 's/^/    /' | sed 's/://'
 
 home:
-	open "https://github.com/sordina/"
+	open "https://github.com/sordina/LoveGame"
 
 issues:
-	open "https://github.com/sordina/"
+	open "https://github.com/sordina/LoveGame/issues"
 
 newissue:
-	open "https://github.com/sordina/"
+	open "https://github.com/sordina/LoveGame/issues/new"
 
 play:
 	love .
--- main.lua
+++ main.lua
@@ -2,18 +2,34 @@ function love.draw()
 	love.graphics.translate(x, y)
 	love.graphics.push()
 	love.graphics.rotate(theta)
-	love.graphics.print("Hello World",0,0)
+	love.graphics.print("Hello -=>" .. bullets,0,0)
 	love.graphics.pop()
 end
 
 function love.mousepressed(x, y, button)
-	bling = love.audio.newSource("music.wav")
+	bling = love.audio.newSource("audio/music.wav")
 	love.audio.play(bling)
+end
+
+function love.keypressed(key,unikey)
+	if key == "escape" then
+		love.event.push("quit")
+	end
+
+	if unikey == 32 then -- space
+		bullets = "=>" .. bullets
+		bling = love.audio.newSource("audio/gun.wav")
+		love.audio.play(bling)
+	end
 end
 
 function love.update(dt)
 	x = x + dt * speed * math.cos(theta)
 	y = y + dt * speed * math.sin(theta)
+
+	if math.random() > 0.9 then
+		bullets = " " .. bullets
+	end
 
 	if love.keyboard.isDown("left") then
 		theta = theta - (dtheta * dt)
@@ -26,7 +42,12 @@ end
 --
 function love.load()
 	init_player()
+	init_bullets()
 	background_music()
+end
+
+function init_bullets()
+	bullets = ""
 end
 
 function init_player()
@@ -38,7 +59,7 @@ function init_player()
 end
 
 function background_music()
-	music = love.audio.newSource("soundtrack01.wav")
+	music = love.audio.newSource("audio/soundtrack01.wav")
 	music:setVolume(0.5) -- 90% of ordinary volume
 	music:setLooping(true)
 	love.audio.play(music)
